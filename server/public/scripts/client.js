5ecdea1e2e68b0db1e261d2d540f2486723cc6b9
Daniel Shugarts <dshugarts@fluidhealthandfitness.com>
2019-09-05 08:51:03 -0500

    bare bones arch

---
+++ server/public/scripts/client.js
@@ -0,0 +1,17 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+/// Routes ///
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  console.log('myApp -- config')
+  $routeProvider
+    .when('/', {
+      redirectTo: 'home'
+    })
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'HomeController as vm',
+    })
+    .otherwise({
+      template: '<h1>404</h1>'
+    });
+}]);
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+let env = require('dotenv');
+env.config();
+
+// Route includes
+
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Filestack Key Call
+const FILESTACK_KEY = process.env.FILESTACK_KEY;
+app.get('/filestack-key', (req, res) => {
+    res.send(FILESTACK_KEY);
+})
+
+// Passport Session Configuration
+app.use(sessionConfig);
+
+// Start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Routes */
+
+
+// Serve static files
+app.use(express.static('server/public'));
+
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
