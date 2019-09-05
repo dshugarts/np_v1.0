b7b9a95468eeecdfeab81e10019823e365af3151
Daniel Shugarts <dshugarts@fluidhealthandfitness.com>
2019-09-05 10:57:30 -0500

    shitty sidenav working

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -2,13 +2,18 @@
 myApp.controller('HomeController', ['$location', function($location) {
     // console.log('LandingController created');
     var self = this;
+  
 
-    self.toDashboard = function() {
-        $location.url('/dashboard')
-      } // end toLogin
+    self.open = function() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
 
-      self.toggle = function() {
-          self.sidebar.toggleClass('active');
-      }
+    self.close = function() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+    }
 
   }]);
